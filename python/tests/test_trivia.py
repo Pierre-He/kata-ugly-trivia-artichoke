2eb7969e736f329634540ba6b20ec5562a569d3c
Demonis0 <demonisgames@gmail.com>
2025-01-15 16:19:31 +0100

    Step 1

---
+++ python/tests/test_trivia.py
@@ -0,0 +1,12 @@
+﻿import trivia
+import pytest
+
+class StubLogger:
+    def __init__(self):
+        self.logs = []
+        
+    def log(self, message):
+        self.logs.append(message)
+
+def test_deterministic():
+    assert trivia.run(StubLogger()).logs == open("./result.txt", "r").read().split("\n")
